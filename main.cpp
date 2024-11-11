f512eeb7108a95cf0dc6370d6d0c68dde6d1af87
Oscar-AVS <alexandervilchis638@gmail.com>
2024-11-10 22:48:10 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+// Este es el progrma principal (main) en donde se implementan todos los códigos creados para cada clase (archivos.h) en este código se le muestra al usuario cada atributo del objeto. 
+// Creado por: Oscar Alexander Vilchis Soto (A01713207)
+// Creado el 10/11/2024
 int main() {
     Deportivo deportivo("Ferrari", "488 GTB", 330, 2023, 250000, "V8", 710);
     Sedan sedan(15.0, "Toyota", "Camry", 2022, 30000, "Automática");
