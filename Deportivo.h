cf8e7c4dba3931c4051372d5b7f2ffb1cef4a839
Oscar-AVS <alexandervilchis638@gmail.com>
2024-11-20 00:07:13 -0600

    Update Deportivo.h

--- Deportivo.h
+++ Deportivo.h
@@ -1,57 +1,31 @@
 // Este es el progrma para la clase "Deportivo.h" en donde se especifica y se expresan los atributos de los objetos pertenecientes a esta clase.
 // Creado por: Oscar Alexander Vilchis Soto (A01713207)
-// Creado el 10/11/2024
+// Creado el 10/11/2024 -  se modificó el 20/11/2024 
+// En esta modificación se cambió el funcionamiento del código ya que ahora hereda distintos atributos de la clase padre Autos.h y mantiene solo ciertos métodos adicionales 
 #ifndef DEPORTIVO_H
 #define DEPORTIVO_H
-#include <iostream>
-#include <string>
-using namespace std;
 
-class Deportivo {
+#include "Auto.h"
+
+class Deportivo : public Auto {
 private:
-    string marca;
-    string modelo;
-    int velocidad_max;
-    int anio;
-    double precio;
     string motor;
-    int caballos_fuerza;
+    int caballosFuerza;
 
 public:
     // Constructor
-    Deportivo(string m, string mod, int vel, int a, double p, string mot, int hp)
-        : marca(m), modelo(mod), velocidad_max(vel), anio(a), precio(p), motor(mot), caballos_fuerza(hp) {}
+    Deportivo(const string& marca, const string& modelo, int anio, float precio, const string& motor, int caballosFuerza)
+        : Auto(marca, modelo, anio, precio), motor(motor), caballosFuerza(caballosFuerza) {}
 
-    // Getters
-    string getMarca() const { return marca; }
-    string getModelo() const { return modelo; }
-    int getVelocidadMax() const { return velocidad_max; }
-    int getAnio() const { return anio; }
-    double getPrecio() const { return precio; }
-    string getMotor() const { return motor; }
-    int getCaballosFuerza() const { return caballos_fuerza; }
-
-    // Setters
-    void setMarca(const string &m) { marca = m; }
-    void setModelo(const string &mod) { modelo = mod; }
-    void setVelocidadMax(int vel) { velocidad_max = vel; }
-    void setAnio(int a) { anio = a; }
-    void setPrecio(double p) { precio = p; }
-    void setMotor(const string &mot) { motor = mot; }
-    void setCaballosFuerza(int hp) { caballos_fuerza = hp; }
-
-    void mostrarInfo() const {
-        cout << "Deportivo: " << marca << " " << modelo << ", del " << anio
-             << ", Velocidad Máxima: " << velocidad_max << " km/h, Precio: $"
-             << precio << ", Motor: " << motor << ", Caballos de Fuerza: "
-             << caballos_fuerza << " hp\n";
+    // Métodos
+    void mostrarInfo() override {
+        Auto::mostrarInfo();
+        cout << "Motor: " << motor << "\nCaballos de Fuerza: " << caballosFuerza << endl;
     }
 
-    void acelerar() const {
-        cout << "El " << marca << " " << modelo << " tiene: "
-             << caballos_fuerza << " caballos de fuerza, alcanzando su velocidad máxima de "
-             << velocidad_max << " km/h.\n";
+    void acelerar() {
+        cout << "El " << modelo << " está acelerando a toda velocidad." << endl;
     }
 };
 
-#endif //DEPORTIVO_H
+#endif // DEPORTIVO_H
