9ba0a0e328ed9678d8acece24e0bc0be0f436bbc
Oscar-AVS <alexandervilchis638@gmail.com>
2024-11-20 00:02:46 -0600

    Update Sedan.h

--- Sedan.h
+++ Sedan.h
@@ -1,53 +1,34 @@
 // Este es el progrma creado para la clase "Sedan.h" en donde se asignan valores y se especifica que atributo tiene el objeto perteneciente a esta clase 
 // Creado por: Oscar Alexander Vilchis Soto (A01713207)
-// Creado el 10/11/2024
+// Creado el 10/11/2024 - Se modificó el 19/11/2024 
+// En esta modificación se cambió el funcionamiento del código ya que ahora hereda distintos atributos de la clase padre Autos.h y mantiene solo ciertos métodos adicionales 
 #ifndef SEDAN_H
 #define SEDAN_H
 #include <iostream>
 #include <string>
 using namespace std;
+#include "Auto.h"
 
-class Sedan {
+class Sedan : public Auto {
 private:
-    double eficiencia_combustible;
-    string marca;
-    string modelo;
-    int anio;
-    double precio;
-    string tipo_transmision;
+    string tipoTransmision;
+    float eficienciaCombustible;
 
 public:
     // Constructor
-    Sedan(double eff, string m, string mod, int a, double p, string transm)
-        : eficiencia_combustible(eff), marca(m), modelo(mod), anio(a), precio(p), tipo_transmision(transm) {}
+    Sedan(const string& marca, const string& modelo, int anio, float precio, const string& tipoTransmision, float eficienciaCombustible)
+        : Auto(marca, modelo, anio, precio), tipoTransmision(tipoTransmision), eficienciaCombustible(eficienciaCombustible) {}
 
-    // Getters
-    double getEficienciaCombustible() const { return eficiencia_combustible; }
-    string getMarca() const { return marca; }
-    string getModelo() const { return modelo; }
-    int getAnio() const { return anio; }
-    double getPrecio() const { return precio; }
-    string getTipoTransmision() const { return tipo_transmision; }
-
-    // Setters
-    void setEficienciaCombustible(double eff) { eficiencia_combustible = eff; }
-    void setMarca(const string &m) { marca = m; }
-    void setModelo(const string &mod) { modelo = mod; }
-    void setAnio(int a) { anio = a; }
-    void setPrecio(double p) { precio = p; }
-    void setTipoTransmision(const string &transm) { tipo_transmision = transm; }
-
-    void mostrarInfo() const {
-        cout << "Sedán: " << marca << " " << modelo << ", Año: " << anio
-             << ", Eficiencia: " << eficiencia_combustible << " km/l, Precio: $"
-             << precio << ", Transmisión: " << tipo_transmision << "\n";
+    // Métodos
+    void mostrarInfo() override {
+        Auto::mostrarInfo();
+        cout << "Transmisión: " << tipoTransmision
+             << "\nEficiencia: " << eficienciaCombustible << " km/l" << endl;
     }
 
-    void calcularRendimiento(double distancia) const {
-        double consumo = distancia / eficiencia_combustible;
-        cout << "Para recorrer " << distancia << " km, el " << marca << " " << modelo
-             << " tiene un consumo de:  " << consumo << " litros de combustible.\n";
+    void calcularRendimiento(float kilometros) {
+        cout << "Rendimiento esperado: " << kilometros / eficienciaCombustible << " litros de combustible." << endl;
     }
 };
+#endif // SEDAN_H
 
-#endif //SEDAN_H
