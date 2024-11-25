8db793f3cf9ec180ca601d6a1a1c5e4a13d96863
Oscar-AVS <alexandervilchis638@gmail.com>
2024-11-24 23:49:02 -0600

    Update Seminuevo.h

--- Seminuevo.h
+++ Seminuevo.h
@@ -1,1 +1,23 @@
+#ifndef SEMINUEVO_H
+#define SEMINUEVO_H
 
+#include "Auto.h"
+
+class Seminuevo : public Auto {
+private:
+    int kilometraje;
+
+public:
+    Seminuevo(string marca, string modelo, int anio, int km, float precio)
+        : Auto(marca, modelo, anio, precio), kilometraje(km) {}
+
+    void mostrarInfo() const override {
+        Auto::mostrarInfo();
+        cout << "Kilometraje: " << kilometraje << " km\n";
+    }
+
+    int getKilometraje() const { return kilometraje; }
+    void setKilometraje(int km) { kilometraje = km; }
+};
+
+#endif
