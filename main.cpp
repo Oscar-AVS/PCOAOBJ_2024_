db4aec73ec84a9082f3a750cbdf30dfd0bcbd439
Oscar-AVS <alexandervilchis638@gmail.com>
2024-11-24 23:53:29 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,24 @@
 // Este es el progrma principal (main) en donde se implementan todos los códigos creados para cada clase (archivos.h) en este código se le muestra al usuario cada atributo del objeto. 
 // Creado por: Oscar Alexander Vilchis Soto (A01713207)
-// Creado el 10/11/2024 - modificado el 19/11/2024
+// Creado el 10/11/2024 - modificado el 19/11/2024- modificado el 24/11/2024
 // Se modificó el código para poder adecuarlo a las nuevas clases de Findmy.h y Auto.h de manera que se pueda implementar en el código la herencia y composición entre clases, este código solo es para llamar a las clases y poder mostrar el resumen al usuario
+// EN la nueva modificación se adaptó el código para que pudiera implementar la interacción con el usuario através de la clase Findmy y de la clase inventario 
+#include <iostream>
+#include "FindMy.h"
+#include "Inventario.h"
+
+using namespace std;
+
 int main() {
-    FindMy app;
+    Inventario inventario;
+    FindMy findMy;
 
-    // Agregar vehículos
-    app.agregarDeportivo();
-    app.agregarSUV();
-    app.agregarSedan();
+    for (const auto& autoObj : inventario.obtenerInventario()) {
+        findMy.agregarAuto(autoObj);
+    }
 
-    // Mostrar vehículos disponibles
-    app.mostrarVehiculos();
-
-    // Pedir datos del usuario
-    app.obtenerDatosUsuario();
-
-    // Mostrar resumen
-    app.mostrarResumen();
+    findMy.interactuarUsuario();
 
     return 0;
 }
+
