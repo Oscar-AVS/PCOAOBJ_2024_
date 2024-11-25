412d36094a45928d2b1c5475769c0b95e09ea66e
Oscar-AVS <alexandervilchis638@gmail.com>
2024-11-25 00:01:34 -0600

    Update SUV.h

--- SUV.h
+++ SUV.h
@@ -1,7 +1,8 @@
 // Este es el progrma para la clase SUV en donde se especifica que atributos tiene dicha clase 
 // Creado por: Oscar Alexander Vilchis Soto (A01713207)
-// Creado el 10/11/2024 - Se modificó el 20/11/2024 
+// Creado el 10/11/2024 - Se modificó el 20/11/2024 - Se modificó el 24/11/2024
 //// En esta modificación se cambió el funcionamiento del código ya que ahora hereda distintos atributos de la clase padre Autos.h y mantiene solo ciertos métodos adicionales 
+// en esta segunda modificación se le añadieron setters que faltabam 
 
 
 #ifndef SUV_H
@@ -11,26 +12,26 @@
 
 class SUV : public Auto {
 private:
+    string traccion;
     int cantidadPasajeros;
     float espacioCajuela;
-    string traccion;
 
 public:
-    // Constructor
-    SUV(const string& marca, const string& modelo, int anio, float precio, int cantidadPasajeros, float espacioCajuela, const string& traccion)
-        : Auto(marca, modelo, anio, precio), cantidadPasajeros(cantidadPasajeros), espacioCajuela(espacioCajuela), traccion(traccion) {}
+    SUV(string marca, string modelo, int anio, string traccion, int pasajeros, float cajuela, float precio)
+        : Auto(marca, modelo, anio, precio), traccion(traccion), cantidadPasajeros(pasajeros), espacioCajuela(cajuela) {}
 
-    // Métodos
-    void mostrarInfo() override {
+    void mostrarInfo() const override {
         Auto::mostrarInfo();
-        cout << "Capacidad de Pasajeros: " << cantidadPasajeros
-             << "\nEspacio de Cajuela: " << espacioCajuela << " m3"
-             << "\nTracción: " << traccion << endl;
+        cout << "Tracción: " << traccion << ", Pasajeros: " << cantidadPasajeros << ", Espacio Cajuela: " << espacioCajuela << " litros\n";
     }
 
-    void activarTraccion() {
-        cout << "La tracción " << traccion << " está activada." << endl;
-    }
+    string getTraccion() const { return traccion; }
+    int getCantidadPasajeros() const { return cantidadPasajeros; }
+    float getEspacioCajuela() const { return espacioCajuela; }
+
+    void setTraccion(const string &traccion) { this->traccion = traccion; }
+    void setCantidadPasajeros(int pasajeros) { cantidadPasajeros = pasajeros; }
+    void setEspacioCajuela(float cajuela) { espacioCajuela = cajuela; }
 };
 
-#endif // SUV_H
+#endif
